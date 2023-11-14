6b2b5c479ab18e59db6779f801d06ee65b04c73b
Parvez Khan <parvezkhan0802@gmail.com>
2023-11-14 22:15:34 +0530

    Palindrome Partitioning

---
+++ Palindrome.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def is_palindrome(sub):
+            return sub == sub[::-1]
+
+        def backtrack(start, path):
+            if start == len(s):
+                result.append(path[:])
+                return
+
+            for end in range(start + 1, len(s) + 1):
+                substring = s[start:end]
+                if is_palindrome(substring):
+                    path.append(substring)
+                    backtrack(end, path)
+                    path.pop()
+
+        result = []
+        backtrack(0, [])
+        return result
